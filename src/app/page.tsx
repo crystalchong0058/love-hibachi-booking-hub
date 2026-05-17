22fa92cdee9047af5f77f3c4b9ed7098a4fb3ec1
Crystal Chong <crystalyschong@gmail.com>
2026-05-17 09:12:16 -0400

    Delete page.tsx

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import ExperiencesSection from '@/components/ExperiencesSection';
-
-      {/* Replace ReviewsSection with ExperiencesSection */}
-      <ExperiencesSection />
-
-      {/* Contact Section */}
-// ... rest of the existing code ... 
